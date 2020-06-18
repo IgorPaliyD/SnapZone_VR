72a55b897d44127486ef645d74e0c62d7685106d
Igor <thekiross@gmail.com>
2020-06-18 19:39:40 +0300

    GatherPointSystem v 0.1

---
+++ Assets/Custom Editor/SanpZone/SetZoneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetZoneController : MonoBehaviour
+{
+   public Dictionary<string,Vector3> listOfGatherPoints;
+    public void SetGatherPoints(){
+       for(int i=0;i<transform.childCount;i++){
+           Transform child = transform.GetChild(i);
+           listOfGatherPoints.Add(child.name,child.position);
+           
+       }
+    }
+
+    
+}
